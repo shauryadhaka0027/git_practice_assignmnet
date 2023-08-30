079c577aec5c932740804a4c938c1f63c496a6db
unknown <shauryadhaka3@gmail.com>
2023-08-30 13:51:59 +0530

    added palindrone code

---
+++ palindrome problem.js
@@ -0,0 +1,5 @@
+let n="masai";
+let bag=""
+for(i=n.length-1;i>=0;i++){
+  bag+=i
+}
---
+++ prime program.js
@@ -0,0 +1,7 @@
+let arr=p[1,3,4,5,67,8];
+let count=0;
+for(i=0;i<arr.length;i++){
+    if(arr[i]%2==1){
+        count++
+    }
+}
